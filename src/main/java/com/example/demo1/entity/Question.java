abb6698aac384e14c9d1edce5211953d23c9b466
LingXY-D <81848295+LingXY-D@users.noreply.github.com>
2021-10-23 18:02:47 +0800

    update

--- src/main/java/com/example/demo1/entity/Question.java
+++ src/main/java/com/example/demo1/entity/Question.java
@@ -6,14 +6,13 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class Question {
-    private int id;           // 题目id
-    private int type;         // 单选、多选或者判断
-    private String stem;      // 题干
-    private String option1;   // 选项
+    private int id;               // 题目id
+    private int stage_id;         // 单选、多选或者判断
+    private String statement;     // 题干
+    private String option1;       // 选项
     private String option2;
     private String option3;
     private String option4;
-    private String answer;    // 答案
-    private int of_level;     // 题目属于的关卡id号
+    private String answer;        // 答案
 }
 
